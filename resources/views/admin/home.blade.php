c70cf59ae5b98efa3a3b3de3c7c95524373a0e24
Richmond Fernandez <escabasjeff@gmail.com>
2024-11-25 21:40:24 +0800

    Richmond changes

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,6 +5,9 @@
         <div class="row">
             <div class="col-md-12">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde veritatis at incidunt perspiciatis, dolorem ut maiores eius a asperiores, repellendus culpa expedita dolor numquam! Voluptatum esse quaerat assumenda at iste!</p>
+                
+                <h2>SAVE ON GITHUB</h2>
+                <p>Hellow world!</p>
             </div>
         </div>
     </div>
